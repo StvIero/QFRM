f43ea0392ef702c89bddbe81f9dbe55aad928f14
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2021-04-08 13:51:13 +0200

    yeet

--- main_maurits.py
+++ main_maurits.py
@@ -14,32 +14,59 @@ import numpy as np
 
 
 # import data
+# assets:
 nikkei = pd.read_csv(r"C:\Users\gebruiker\Documents\GitHub\QFRM\NIKKEI_225.csv")
 jse = pd.read_csv(r"C:\Users\gebruiker\Documents\GitHub\QFRM\JSE_TOP40.csv")
 aex = pd.read_csv(r"C:\Users\gebruiker\Documents\GitHub\QFRM\AEX.csv")
+#jse['Last'] = pd.to_numeric(jse['Last'])
+
+
+# fx:
+euryen = pd.read_csv(r"C:\Users\gebruiker\Documents\GitHub\QFRM\EUR_YEN.csv")
+eurzar = pd.read_csv(r"C:\Users\gebruiker\Documents\GitHub\QFRM\EUR_ZAR.csv")
+
+# debt:
 
 # change dates to datetime, trim and set datetime as index:
 nikkei['Date'] = pd.to_datetime(nikkei['Date'])
 jse['Date'] = pd.to_datetime(jse['Date'])
 aex['Date'] = pd.to_datetime(aex['Date'])
+nikkei['Date2'] = nikkei['Date']
+jse['Date2'] = jse['Date']
+aex['Date2'] = aex['Date']
 
-nikkei = pd.DataFrame(
-        {'n_price': np.array(nikkei['Price']),
-         'Date': np.array(nikkei['Date'])
-                })
-jse = pd.DataFrame(
-        {'j_price': np.array(jse['Settlement Price']), #is it settlement price though?
-         'Date': np.array(jse['Date'])
-                })
-aex = pd.DataFrame(
-        {'a_price': np.array(aex['Price']),
-         'Date': np.array(aex['Date'])
-                })
+euryen['Date'] = pd.to_datetime(euryen['Date'])
+eurzar['Date'] = pd.to_datetime(eurzar['Date'])
+euryen.set_index('Date', inplace=True)
+eurzar.set_index('Date', inplace=True)
 
 dates = nikkei['Date']
 
 nikkei.set_index('Date', inplace=True)
 jse.set_index('Date', inplace=True)
+aex.set_index('Date', inplace=True)
+
+nikkei = pd.merge(nikkei, euryen, left_index=True, right_index=True)
+jse = pd.merge(jse, eurzar, left_index=True, right_index=True)
+
+nikkei['price_eur'] = nikkei['Price']*nikkei['Mid']
+jse['price_eur'] = jse['Last']*jse['Mid']
+
+nikkei = pd.DataFrame(
+        {'n_price': np.array(nikkei['price_eur']),
+         'Date': np.array(nikkei['Date2'])
+                })
+jse = pd.DataFrame(
+        {'j_price': np.array(jse['price_eur']), 
+         'Date': np.array(jse['Date2'])
+                })
+aex = pd.DataFrame(
+        {'a_price': np.array(aex['Price']),
+         'Date': np.array(aex['Date2'])
+                })
+
+nikkei.set_index('Date', inplace=True) # kind of roundaboutish and hacky but
+jse.set_index('Date', inplace=True)    # I just want this to run properly now
 aex.set_index('Date', inplace=True)
 
 # create new master df with all mfing uuuuhhh price series...
