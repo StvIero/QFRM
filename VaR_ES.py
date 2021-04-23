df0f7eef4d55f33f88acaa3258e20c11d7a198b0
Connor <c.m.stevens@student.vu.nl>
2021-04-23 17:47:10 +0200

    VaR and ES calculations for absolute weights.

--- VaR_ES.py
+++ VaR_ES.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import arch
 
 #Import loss dataframe.
 df_loss = pd.read_csv('/Users/connorstevens/Documents/GitHub/QFRM/Data/loss_df.csv', index_col= 0)
 
-
+garch = arch_model(returns, vol='garch', p=1, o=0, q=1)
+garch_fitted = garch.fit()
 
