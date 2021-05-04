bd125063110524a7d7a7be8626f6310c3024a442
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2021-05-04 16:09:02 +0200

    updated datapuller and started basics of PCA

--- assignment_3_main.py
+++ assignment_3_main.py
@@ -13,7 +13,27 @@ import os
 os.chdir(r"C:\Users\gebruiker\Documents\GitHub\QFRM") # necessary to get dataloader function
 from data_puller3000 import DataPuller_assignment3 # get dataloader function
 
+
+# start PCA analysis here, kinda want to have it set up in a way so i can automate it more easily next time:
+def PCA(df, ticker_list):
+    # packages again:
+    from sklearn.decomposition import PCA
+        
+    ticker_list = ['ASML.AS', 'SONY', 'AKZA.AS', 'BAYN.DE', 'TSN', 'NTDOY', 'SQNXF', 'AMD','CSGN.SW', 'MUFG']
+    dfrets = df.iloc[1:,len(ticker_list)+1:len(df.columns)-1]
+    
+    # now just use PCA analysis on the dfrets to see some dependencies:
+    pca = PCA(n_components='mle', svd_solver = 'full', )
+    pca_fit = pca.fit(dfrets)
+    
+    return pca_fit
+
+
+
+
+'''====================================== RUN HERE =================================='''
+
 df = DataPuller_assignment3() # get data
 
-# start PCA analysis here:
 
+test = PCA(df, 0)
